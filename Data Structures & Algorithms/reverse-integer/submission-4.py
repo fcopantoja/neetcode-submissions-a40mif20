f6eba7ccce7082f020de17621fb46778e733ccc4
Francisco Pantoja <fcopantoja@gmail.com>
2026-04-11 07:36:55 -0600

    Add: reverse-integer - submission-4

---
+++ Data Structures & Algorithms/reverse-integer/submission-4.py
@@ -0,0 +1,18 @@
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        y = abs(x)
+        res = 0
+        while y:
+            digit = y % 10
+            res = res * 10 + digit
+            y = y // 10
+        
+        if not res:
+            return 0
+        
+        res = res if x > 0 else -res
+        if res > (2 ** 31) or res < (-2 ** 31):
+            return 0
+        
+        return res
